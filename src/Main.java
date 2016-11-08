c64c69f34bd745cdadb835be72bed35a180baa24
attoff <viktor.attoff@gmail.com>
2016-11-08 15:46:02 +0100

    Small example works pretty good. Large example gives to high values. Don't know why yet.

---
+++ src/Main.java
@@ -0,0 +1,32 @@
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        MicroMint mimi = new MicroMint();
+        int total1Rounds = 0;
+        int total100Rounds = 0;
+        int total10kRounds = 0;
+        int u = 20;
+        int k = 7;
+        int round = 0;
+        int totalTries = 100;
+
+        for (int i = 0; i < totalTries; i++) {
+            round = mimi.getRoundsFor(u, k, 1);
+            total1Rounds += round;
+            round = mimi.getRoundsFor(u, k, 100);
+            total100Rounds += round;
+            round = mimi.getRoundsFor(u, k, 10000);
+            total10kRounds += round;
+            System.out.println(i);
+        }
+
+        System.out.println("The mean for 1 coin is: " + total1Rounds/totalTries );
+        System.out.println("The mean for 100 coins is: " + total100Rounds/totalTries );
+        System.out.println("The mean for 10k coins is: " + total10kRounds/totalTries );
+    }
+
+
+}
--- src/MicroMint.java
+++ src/MicroMint.java
@@ -1,5 +1,48 @@
-/**
- * Created by vikto on 2016-11-08.
- */
+import java.util.ArrayList;
+import java.util.Random;
+
+
 public class MicroMint {
+    private Random rand;
+    private int[] bins;
+    private ArrayList<Integer> usedCoins;
+    private int nbrRounds;
+    private int nbrCoins;
+    private int nbrBins;
+
+    public MicroMint() {
+
+    }
+
+
+    public int getRoundsFor(int u, int k, int c) {
+        rand = new Random();
+        usedCoins = new ArrayList<>();
+        nbrBins = (int) Math.pow(2, u);
+        nbrCoins = 0;
+        nbrRounds = 0;
+        bins = new int[nbrBins];
+
+        while (nbrCoins < c) {
+            int bin = rand.nextInt(nbrBins);
+
+            //while (usedCoins.contains(bin)){
+            //    bin = rand.nextInt(nbrBins);
+            //}
+
+            bins[bin]++;
+
+            if (bins[bin] == k) {
+                //usedCoins.add(bin);
+                nbrCoins++;
+                bins[bin] = 0;
+            }
+            nbrRounds++;
+        }
+
+
+        return nbrRounds;
+    }
+
+
 }
