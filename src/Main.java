d1a3a5be93574084dce6f35ea9fea05c52fda554
attoff <viktor.attoff@gmail.com>
2016-11-10 13:31:11 +0100

    Fixed commented code, should work as wanted now.

--- src/Main.java
+++ src/Main.java
@@ -7,21 +7,19 @@ public class Main {
         MicroMint mimi = new MicroMint();
         int total1Rounds = 0;
         int total100Rounds = 0;
-        int total10kRounds = 0;
+        long total10kRounds = 0;
         int u = 20;
         int k = 7;
-        int round = 0;
-        int totalTries = 10000;
+        long round = 0;
+        int totalTries = 1;
 
         for (int i = 0; i < totalTries; i++) {
             round = mimi.getRoundsFor(u, k, 1);
             total1Rounds += round;
-           /** round = mimi.getRoundsFor(u, k, 100);
+            round = mimi.getRoundsFor(u, k, 100);
             total100Rounds += round;
             round = mimi.getRoundsFor(u, k, 10000);
             total10kRounds += round;
-            */
-            System.out.println(i);
         }
 
         System.out.println("The mean for 1 coin is: " + total1Rounds/totalTries );
--- src/MicroMint.java
+++ src/MicroMint.java
@@ -5,7 +5,6 @@ import java.util.Random;
 public class MicroMint {
     private Random rand;
     private int[] bins;
-    private int[] usedCoins;
     private int nbrRounds;
     private int nbrCoins;
     private int nbrBins;
@@ -15,32 +14,21 @@ public class MicroMint {
     }
 
 
-    public int getRoundsFor(int u, int k, int c) {
+    public long getRoundsFor(int u, int k, int c) {
         rand = new Random();
         nbrBins = (int) Math.pow(2, u);
         nbrCoins = 0;
         nbrRounds = 0;
         bins = new int[nbrBins];
-        usedCoins = new int[nbrBins];
 
         while (nbrCoins < c) {
             int bin = rand.nextInt(nbrBins);
-
-            while (usedCoins[bin]==1){
-                bin = rand.nextInt(nbrBins);
-            }
-
             bins[bin]++;
-
             if (bins[bin] == k) {
-                usedCoins[bin]=1;
                 nbrCoins++;
-                //bins[bin] = 0;
             }
             nbrRounds++;
         }
-
-
         return nbrRounds;
     }
 
